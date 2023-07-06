f33723bd64f365e4869241741c349ab0ea44521c
tanius <matthias@ansorgs.de>
2023-07-07 01:37:36 +0200

    major update: storing project backings, benefits and ITMO accounting info

--- src-supabase/3-fkey-constraints.sql
+++ src-supabase/3-fkey-constraints.sql
@@ -1,7 +1,46 @@
 --
--- Project-defined foreign key constraints
+-- Foreign key constraints for tables defined by the project
 -- (Other constraints that do not depend on table creation order and are in 1-tables.sql.)
 --
+
+-- benefit
+
+ALTER TABLE ONLY public.benefit
+    ADD CONSTRAINT benefit_project_id_fkey 
+        FOREIGN KEY (project_id) REFERENCES public.project(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.benefit
+    DROP CONSTRAINT IF EXISTS benefit_beneficiary_fkey,
+    ADD CONSTRAINT benefit_beneficiary_fkey 
+        FOREIGN KEY (beneficiary) REFERENCES public.profile(id) ON DELETE RESTRICT;
+
+ALTER TABLE ONLY public.benefit
+    DROP CONSTRAINT IF EXISTS benefit_evidence_fkey,
+    ADD CONSTRAINT benefit_evidence_fkey 
+        FOREIGN KEY (evidence) REFERENCES public.medium(id) ON DELETE SET NULL;
+
+ALTER TABLE ONLY public.backing
+    DROP CONSTRAINT IF EXISTS backing_support_type_fkey,
+    ADD CONSTRAINT backing_support_type_fkey 
+        FOREIGN KEY (support_type) REFERENCES public.tag(id) ON DELETE SET NULL;
+
+-- benefit_type
+
+ALTER TABLE ONLY public.benefit_type
+    ADD CONSTRAINT benefit_type_benefit_id_fkey 
+        FOREIGN KEY (benefit_id) REFERENCES public.benefit(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.benefit_type
+    ADD CONSTRAINT benefit_type_tag_id_fkey 
+        FOREIGN KEY (tag_id) REFERENCES public.tag(id) ON DELETE CASCADE;
+
+
+-- emissions_avoided
+
+ALTER TABLE ONLY public.emissions_avoided
+    DROP CONSTRAINT IF EXISTS emissions_avoided_project_id_fkey,
+    ADD CONSTRAINT emissions_avoided_project_id_fkey FOREIGN KEY (project_id) 
+        REFERENCES public.project(id) ON DELETE CASCADE;
 
 -- medium
 
@@ -15,13 +54,13 @@ ALTER TABLE ONLY public.medium
 
 ALTER TABLE ONLY public.medium
     DROP CONSTRAINT IF EXISTS medium_programme_id_fkey,
-    ADD CONSTRAINT medium_programme_id_fkey FOREIGN KEY (programme_id) 
-        REFERENCES public.programme(id);
+    ADD CONSTRAINT medium_programme_id_fkey 
+        FOREIGN KEY (programme_id) REFERENCES public.programme(id);
 
 ALTER TABLE ONLY public.medium
     DROP CONSTRAINT IF EXISTS medium_programme_step_id_fkey,
-    ADD CONSTRAINT medium_programme_step_id_fkey FOREIGN KEY (programme_step_id) 
-        REFERENCES public.step(id);
+    ADD CONSTRAINT medium_programme_step_id_fkey 
+        FOREIGN KEY (programme_step_id) REFERENCES public.step(id);
 
 ALTER TABLE ONLY public.medium
     DROP CONSTRAINT IF EXISTS medium_project_id_fkey,
@@ -39,24 +78,24 @@ ALTER TABLE ONLY public.profile
 
 ALTER TABLE ONLY public.programme_response
     DROP CONSTRAINT IF EXISTS programme_response_programme_step_id_fkey,
-    ADD CONSTRAINT programme_response_programme_step_id_fkey FOREIGN KEY (programme_step_id) 
-        REFERENCES public.programme_step(id);
+    ADD CONSTRAINT programme_response_programme_step_id_fkey 
+        FOREIGN KEY (programme_step_id) REFERENCES public.programme_step(id);
 
 
 -- programme_step
 
 ALTER TABLE ONLY public.programme_step
     DROP CONSTRAINT IF EXISTS programme_step_programme_id_fkey,
-    ADD CONSTRAINT programme_step_programme_id_fkey FOREIGN KEY (programme_id) 
-        REFERENCES public.programme(id);
+    ADD CONSTRAINT programme_step_programme_id_fkey 
+        FOREIGN KEY (programme_id) REFERENCES public.programme(id);
 
 
 -- project
 
 ALTER TABLE ONLY public.project
     DROP CONSTRAINT IF EXISTS project_programme_id_fkey,
-    ADD CONSTRAINT project_programme_id_fkey FOREIGN KEY (programme_id) 
-        REFERENCES public.programme(id);
+    ADD CONSTRAINT project_programme_id_fkey 
+        FOREIGN KEY (programme_id) REFERENCES public.programme(id);
 
 
 -- response
@@ -71,8 +110,8 @@ ALTER TABLE ONLY public.response
 
 ALTER TABLE ONLY public.response
     DROP CONSTRAINT IF EXISTS response_programme_response_id_fkey,
-    ADD CONSTRAINT response_programme_response_id_fkey FOREIGN KEY (programme_response_id) 
-        REFERENCES public.programme_response(id);
+    ADD CONSTRAINT response_programme_response_id_fkey 
+        FOREIGN KEY (programme_response_id) REFERENCES public.programme_response(id);
 
 ALTER TABLE ONLY public.response
     DROP CONSTRAINT IF EXISTS response_step_id_fkey,
@@ -102,8 +141,8 @@ ALTER TABLE ONLY public.step
 
 ALTER TABLE ONLY public.step
     DROP CONSTRAINT IF EXISTS step_programme_step_id_fkey,
-    ADD CONSTRAINT step_programme_step_id_fkey FOREIGN KEY (programme_step_id) 
-        REFERENCES public.programme_step(id);
+    ADD CONSTRAINT step_programme_step_id_fkey 
+        FOREIGN KEY (programme_step_id) REFERENCES public.programme_step(id);
 
 ALTER TABLE ONLY public.step
     DROP CONSTRAINT IF EXISTS step_project_id_fkey,
